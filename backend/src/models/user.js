550c44b7da8d9fffa57ac7b3d58c9453ac310b8c
sam.webdev105 <sam.webdev105@gmail.com>
2024-07-17 19:29:37 -0400

    set user token default value as 100

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -4,7 +4,7 @@ const userSchema = mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  crypto_token: { type: Number, required: true },
+  crypto_token: { type: Number, required: true, default: 100 },
   id: { type: String },
 });
 
